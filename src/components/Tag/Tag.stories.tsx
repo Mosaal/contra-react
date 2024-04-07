7a20345cb39ccd058f4e2d5ab1b0ff73d584a61b
Alexandre Saraiva Moreira <alexandremoreira1995@gmail.com>
2024-04-07 19:16:25 +0100

    CR: feat - Added Tag

---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,34 @@
+import { fn } from "@storybook/test";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Tag from "./Tag";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta: Meta<typeof Tag> = {
+  title: "Tag",
+  component: Tag,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Inactive: Story = {
+  args: {
+    label: "Inactive Tag",
+  },
+};
+
+export const Active: Story = {
+  args: {
+    label: "Active Tag",
+    active: true,
+    onDeactivate: fn(),
+  },
+};
---
+++ src/components/Tag/index.ts
@@ -0,0 +1,3 @@
+export * from "./Tag";
+export * from "./Tag.types";
+export { default as Tag } from "./Tag";

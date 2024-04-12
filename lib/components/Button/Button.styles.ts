1a2360a210e7dab3096be38e17f96795dc62ff57
Alexandre Moreira <alexandremoreira1995@gmail.com>
2024-04-13 00:30:42 +0100

    CR: chore - Moved the button colors to a shared object

--- lib/components/Button/Button.styles.ts
+++ lib/components/Button/Button.styles.ts
@@ -1,19 +1,21 @@
 import { cva } from "class-variance-authority";
 
+export const BUTTON_COLORS = {
+  primary:
+    "border-contra-black bg-contra-white text-contra-black disabled:border-contra-black-300 disabled:text-contra-black-300",
+  secondary:
+    "border-contra-black bg-contra-black text-contra-white disabled:border-contra-black-300 disabled:bg-contra-black-300",
+  tertiary:
+    "border-contra-blue bg-contra-blue text-contra-white disabled:border-contra-blue-800 disabled:bg-contra-blue-800",
+  quaternary:
+    "border-contra-black bg-contra-yellow text-contra-black disabled:border-contra-black-300 disabled:bg-contra-yellow-100 disabled:text-contra-black-300",
+} as const;
+
 export const buttonCva = cva(
   "appearance-none border-0.5 font-extrabold shadow-contra-black transition disabled:cursor-not-allowed disabled:shadow-contra-black-300",
   {
     variants: {
-      variant: {
-        primary:
-          "border-contra-black bg-contra-white text-contra-black disabled:border-contra-black-300 disabled:text-contra-black-300",
-        secondary:
-          "border-contra-black bg-contra-black text-contra-white disabled:border-contra-black-300 disabled:bg-contra-black-300",
-        tertiary:
-          "border-contra-blue bg-contra-blue text-contra-white disabled:border-contra-blue-800 disabled:bg-contra-blue-800",
-        quaternary:
-          "border-contra-black bg-contra-yellow text-contra-black disabled:border-contra-black-300 disabled:bg-contra-yellow-100 disabled:text-contra-black-300",
-      },
+      variant: BUTTON_COLORS,
       size: {
         small: "rounded-3 px-3.5 py-2 text-3 leading-4",
         normal: "rounded-4 px-3.5 py-3.5 text-5.25 leading-7",
--- lib/components/IconButton/IconButton.styles.ts
+++ lib/components/IconButton/IconButton.styles.ts
@@ -1,19 +1,12 @@
 import { cva } from "class-variance-authority";
 
+import { BUTTON_COLORS } from "../Button";
+
 export const iconButtonCva = cva(
   "appearance-none rounded-full border-0.5 shadow-contra-black transition disabled:cursor-not-allowed disabled:shadow-contra-black-300",
   {
     variants: {
-      variant: {
-        primary:
-          "border-contra-black bg-contra-white text-contra-black disabled:border-contra-black-300 disabled:text-contra-black-300",
-        secondary:
-          "border-contra-black bg-contra-black text-contra-white disabled:border-contra-black-300 disabled:bg-contra-black-300",
-        tertiary:
-          "border-contra-blue bg-contra-blue text-contra-white disabled:border-contra-blue-800 disabled:bg-contra-blue-800",
-        quaternary:
-          "border-contra-black bg-contra-yellow text-contra-black disabled:border-contra-black-300 disabled:bg-contra-yellow-100 disabled:text-contra-black-300",
-      },
+      variant: BUTTON_COLORS,
       size: {
         small: "h-8 w-8",
         normal: "h-12 w-12",

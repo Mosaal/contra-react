4dd36a53794fd78c9b979c5a914d012a861181e0
Alexandre Moreira <alexandremoreira1995@gmail.com>
2024-04-13 18:55:41 +0100

    CR: chore(Stepper) - Moved Stepper component into the correct foled

--- lib/components/Stepper/Stepper.tsx
+++ lib/components/Stepper/Stepper.tsx
@@ -4,7 +4,7 @@ import type { StepperProps } from "./Stepper.types";
 
 import { Plus, Minus } from "@/icons";
 
-import { cn, useCn, useInputId } from "@/utils";
+import { cn, useInputId } from "@/utils";
 
 const Stepper = forwardRef<HTMLInputElement, StepperProps>(function (
   {
@@ -32,23 +32,23 @@ const Stepper = forwardRef<HTMLInputElement, StepperProps>(function (
 
   const handleIncrement = () => {
     setValue(value + 1);
-    onDecrement?.(value + 1);
+    onIncrement?.(value + 1);
   };
 
   const renderInsideSingle = () => {
     return (
       <label
         htmlFor={inputId}
-        className="flex flex-row items-center rounded-4 border-0.5 border-contra-black overflow-hidden"
+        className="flex flex-row items-center overflow-hidden rounded-4 border-0.5 border-contra-black"
       >
-        <span className="py-2 pl-5.5 pr-3.5 text-6 leading-7 font-extrabold text-contra-black bg-contra-yellow">
+        <span className="bg-contra-yellow py-2 pl-5.5 pr-3.5 text-6 font-extrabold leading-7 text-contra-black">
           {label || "Add"}
         </span>
         <button
-          className="py-2.5 pl-2 pr-2.25 bg-contra-white"
+          className="bg-contra-white py-2.5 pl-2 pr-2.25"
           onClick={handleIncrement}
         >
-          <Plus strokeWidth={4} className="w-6 h-6" />
+          <Plus strokeWidth={4} className="h-6 w-6" />
         </button>
         <input
           hidden
@@ -66,22 +66,22 @@ const Stepper = forwardRef<HTMLInputElement, StepperProps>(function (
     return (
       <label
         htmlFor={inputId}
-        className="flex flex-row items-center rounded-4 border-0.5 border-contra-black overflow-hidden"
+        className="flex flex-row items-center overflow-hidden rounded-4 border-0.5 border-contra-black"
       >
         <button
-          className="py-2.5 pr-2 pl-2.25 bg-contra-white"
+          className="bg-contra-white py-2.5 pl-2.25 pr-2"
           onClick={handleDecrement}
         >
-          <Minus strokeWidth={4} className="w-6 h-6" />
+          <Minus strokeWidth={4} className="h-6 w-6" />
         </button>
-        <span className="py-2 w-12 text-6 leading-7 font-extrabold text-center text-contra-black bg-contra-yellow">
+        <span className="w-12 bg-contra-yellow py-2 text-center text-6 font-extrabold leading-7 text-contra-black">
           {label || value}
         </span>
         <button
-          className="py-2.5 pl-2 pr-2.25 bg-contra-white"
+          className="bg-contra-white py-2.5 pl-2 pr-2.25"
           onClick={handleIncrement}
         >
-          <Plus strokeWidth={4} className="w-6 h-6" />
+          <Plus strokeWidth={4} className="h-6 w-6" />
         </button>
       </label>
     );
@@ -100,21 +100,21 @@ const Stepper = forwardRef<HTMLInputElement, StepperProps>(function (
         </span>
         <button
           className={cn(
-            "appearance-none border-0.5 border-contra-black text-contra-black bg-contra-yellow flex items-center justify-center",
-            size === "small" ? "w-9 h-9 rounded-3" : "w-12 h-12 rounded-4",
+            "flex appearance-none items-center justify-center border-0.5 border-contra-black bg-contra-yellow text-contra-black",
+            size === "small" ? "h-9 w-9 rounded-3" : "h-12 w-12 rounded-4",
           )}
           onClick={direction === "positive" ? handleIncrement : handleDecrement}
         >
           {(direction === "positive" && (
             <Plus
               strokeWidth={4}
-              className={size === "small" ? "w-4 h-4" : "w-6 h-6"}
+              className={size === "small" ? "h-4 w-4" : "h-6 w-6"}
             />
           )) ||
             (direction === "negative" && (
               <Minus
                 strokeWidth={4}
-                className={size === "small" ? "w-4 h-4" : "w-6 h-6"}
+                className={size === "small" ? "h-4 w-4" : "h-6 w-6"}
               />
             ))}
         </button>
@@ -133,21 +133,21 @@ const Stepper = forwardRef<HTMLInputElement, StepperProps>(function (
   const renderOutsideDouble = () => {
     return (
       <label htmlFor={inputId} className="flex flex-row items-center gap-x-3">
-        <span className="text-9 leading-10 font-extrabold text-contra-black">
+        <span className="text-9 font-extrabold leading-10 text-contra-black">
           {label || value}
         </span>
         <div className="flex flex-row items-center">
           <button
-            className="appearance-none w-12 h-12 rounded-l-4 border-l-0.5 border-r-0.25 border-y-0.5 border-contra-black text-contra-black bg-contra-yellow flex items-center justify-center"
+            className="flex h-12 w-12 appearance-none items-center justify-center rounded-l-4 border-y-0.5 border-l-0.5 border-r-0.25 border-contra-black bg-contra-yellow text-contra-black"
             onClick={handleDecrement}
           >
-            <Minus strokeWidth={4} className="w-6 h-6" />
+            <Minus strokeWidth={4} className="h-6 w-6" />
           </button>
           <button
-            className="appearance-none w-12 h-12 rounded-r-4 border-r-0.5 border-l-0.25 border-y-0.5 border-contra-black text-contra-black bg-contra-yellow flex items-center justify-center"
+            className="flex h-12 w-12 appearance-none items-center justify-center rounded-r-4 border-y-0.5 border-l-0.25 border-r-0.5 border-contra-black bg-contra-yellow text-contra-black"
             onClick={handleIncrement}
           >
-            <Plus strokeWidth={4} className="w-6 h-6" />
+            <Plus strokeWidth={4} className="h-6 w-6" />
           </button>
         </div>
         <input
